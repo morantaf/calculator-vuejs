928e5da8f7a7be85d151f4a91459c9f4acd8bba9
morantaf <fall_moranta@live.fr>
2020-08-05 16:12:29 +0200

    Removing the array of values in data to replace it by several button tags each assigned with a value. This has for objective to try to understand how the event handler  works and to understand better how the parent and the child interact with each other

--- src/components/Keys.vue
+++ src/components/Keys.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <button v-for="(value, index) in values" v-bind:key="index">{{ value }}</button>
+    <button value="0" v-on:click="onClick($event)">0</button>
+    <button value="1" v-on:click="onClick($event)">1</button>
+    <button value="2" v-on:click="onClick($event)">2</button>
+    <button value="3" v-on:click="onClick($event)">3</button>
+    <button value="4" v-on:click="onClick($event)">4</button>
+    <button value="5" v-on:click="onClick($event)">5</button>
+    <button value="6" v-on:click="onClick($event)">6</button>
+    <button value="7" v-on:click="onClick($event)">7</button>
+    <button value="8" v-on:click="onClick($event)">8</button>
+    <button value="9" v-on:click="onClick($event)">9</button>
+    <button value="." v-on:click="onClick($event)">.</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["values"],
+  methods: {
+    onClick(e) {
+      this.$emit("type-key", e.target.value);
+    },
+  },
 };
 </script>
 
