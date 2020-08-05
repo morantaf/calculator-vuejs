289163c19c47cf9ef0bd38eee4dfafe5e8a4d062
morantaf <fall_moranta@live.fr>
2020-08-05 14:29:26 +0200

    creating The Keys component that'll populate the app with buttons with values

---
+++ src/components/Keys.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button v-for="(value, index) in values" v-bind:key="index">{{ value }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["values"],
+};
+</script>
+
+<style></style>
